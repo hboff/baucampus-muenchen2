6ed1942b635e0e4d03688e7a4ec209054f54d3ca
Oskar Lohse <oskar@baucampus.de>
2023-07-03 21:27:47 +0200

    test

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\View;
+use App\Models\Ort;
+use App\Models\Orteat;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +26,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        View::share('ort', Ort::all());
+        View::share('ortat', Orteat::all());
     }
 }
