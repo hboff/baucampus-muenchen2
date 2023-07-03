7403dabc4f60c8f99dd2db85908f3617384069fe
Oskar Lohse <oskar@baucampus.de>
2023-07-03 16:27:51 +0200

    test 3

--- resources/views/leistungen/berwertung.blade.php
+++ resources/views/leistungen/berwertung.blade.php
@@ -13,24 +13,24 @@
   <meta content="summary_large_image" name="twitter:card">
   <meta content="width=device-width, initial-scale=1" name="viewport">
   <meta content="Webflow" name="generator">
-  <link href="../css/normalize.css" rel="stylesheet" type="text/css">
-  <link href="../css/webflow.css" rel="stylesheet" type="text/css">
-  <link href="../css/baucampus-fr-3d2c130f2c9eb3bf5def5f18fb.webflow.css" rel="stylesheet" type="text/css">
+  <link href="/css/normalize.css" rel="stylesheet" type="text/css">
+  <link href="/css/webflow.css" rel="stylesheet" type="text/css">
+  <link href="/css/baucampus-fr-3d2c130f2c9eb3bf5def5f18fb.webflow.css" rel="stylesheet" type="text/css">
   <link href="https://fonts.googleapis.com" rel="preconnect">
   <link href="https://fonts.gstatic.com" rel="preconnect" crossorigin="anonymous">
   <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js" type="text/javascript"></script>
   <script type="text/javascript">WebFont.load({  google: {    families: ["Great Vibes:400"]  }});</script>
   <script type="text/javascript">!function(o,c){var n=c.documentElement,t=" w-mod-";n.className+=t+"js",("ontouchstart"in o||o.DocumentTouch&&c instanceof DocumentTouch)&&(n.className+=t+"touch")}(window,document);</script>
-  <link href="../images/favicon.png" rel="shortcut icon" type="image/x-icon">
-  <link href="../images/webclip.webp" rel="apple-touch-icon"><!--  Finsweet Cookie Consent  -->
+  <link href="/images/favicon.png" rel="shortcut icon" type="image/x-icon">
+  <link href="/images/webclip.webp" rel="apple-touch-icon"><!--  Finsweet Cookie Consent  -->
   <script async="" src="https://cdn.jsdelivr.net/npm/@finsweet/cookie-consent@1/fs-cc.js" fs-cc-mode="opt-in"></script>
   <script type="fs-cc" id="hs-script-loader" async="" defer="" src="https://js-eu1.hs-scripts.com/26454454.js" fs-cc-categories="marketing"></script>
 </head>
 <body class="body">
   <div data-collapse="medium" data-animation="default" data-duration="400" data-w-id="9962d9cc-619f-1e92-7f2b-747f23a87122" data-easing="ease" data-easing2="ease" role="banner" class="navbar w-nav">
     <div class="container flex">
-      <a href="..//" class="brand w-nav-brand">
-        <div class="logo-wrapper"><img src="../images/logo.png" loading="lazy" width="49" sizes="49px" srcset="../images/logo-p-500.png 500w, ../images/logo-p-800.png 800w, ../images/logo-p-1080.png 1080w, ../images/logo.png 1172w" alt="">
+      <a href="//" class="brand w-nav-brand">
+        <div class="logo-wrapper"><img src="/images/logo.png" loading="lazy" width="49" sizes="49px" srcset="/images/logo-p-500.png 500w, /images/logo-p-800.png 800w, /images/logo-p-1080.png 1080w, /images/logo.png 1172w" alt="">
           <div class="text-logo-wrapper">
             <div class="logo-text">Baucampus<br></div>
             <div class="logo-text subtext">München</div>
@@ -38,44 +38,44 @@
         </div>
       </a>
       <nav role="navigation" class="nav-menu w-nav-menu">
-        <a href="..//" class="nav-link w-nav-link">Home</a>
+        <a href="//" class="nav-link w-nav-link">Home</a>
         <div data-hover="true" data-delay="300" data-w-id="9962d9cc-619f-1e92-7f2b-747f23a8712f" class="navbar-menu-dropdown w-dropdown">
           <aside class="dropdown-toggle w-dropdown-toggle">
             <div class="dropdown-icon w-embed"><svg width="20" height="20" viewbox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M5 7.5L10 12.5L15 7.5" stroke="currentColor" stroke-width="1.67" stroke-linecap="round" stroke-linejoin="round"></path>
               </svg></div>
-            <a href="../leistungen" class="link-for-dropdown w-inline-block">
+            <a href="/leistungen" class="link-for-dropdown w-inline-block">
               <div>Dienstleistungen</div>
             </a>
           </aside>
           <nav class="dropdown-list w-dropdown-list">
             <div class="dropdown-link-list">
-              <a href="../dienstleistungen/bauschaden" class="dropdown-link w-inline-block">
+              <a href="/dienstleistungen/bauschaden" class="dropdown-link w-inline-block">
                 <div>
                   <div>Beratung Bauschaden</div>
                 </div>
               </a>
-              <a href="../dienstleistungen/schimmelpilz" class="dropdown-link w-inline-block">
+              <a href="/dienstleistungen/schimmelpilz" class="dropdown-link w-inline-block">
                 <div>
                   <div>Beratung Schimmmelpilz</div>
                 </div>
               </a>
-              <a href="../dienstleistungen/hauskaufberatung" class="dropdown-link w-inline-block">
+              <a href="/dienstleistungen/hauskaufberatung" class="dropdown-link w-inline-block">
                 <div>
                   <div>Kaufberatung</div>
                 </div>
               </a>
-              <a href="../dienstleistungen/baubegleitung" class="dropdown-link w-inline-block">
+              <a href="/dienstleistungen/baubegleitung" class="dropdown-link w-inline-block">
                 <div>
                   <div>Baubegleitung</div>
                 </div>
               </a>
-              <a href="../dienstleistungen/energieberatung" class="dropdown-link w-inline-block">
+              <a href="/dienstleistungen/energieberatung" class="dropdown-link w-inline-block">
                 <div>
                   <div>Energieberatung</div>
                 </div>
               </a>
-              <a href="../dienstleistungen/immobilienbewertung" class="dropdown-link w-inline-block">
+              <a href="/dienstleistungen/immobilienbewertung" class="dropdown-link w-inline-block">
                 <div id="w-node-c8909249-ed55-ded4-b3d1-381c167e4fbc-23a87122">
                   <div>Immobilienbewertung</div>
                 </div>
@@ -84,8 +84,8 @@
           </nav>
         </div>
         <a href="https://baucampus.com" target="_blank" class="nav-link w-nav-link">Baucampus</a>
-        <a href="../preise" class="nav-link w-nav-link">Preise</a>
-        <a href="../kontakt" class="nav-link w-nav-link">Kontakt</a>
+        <a href="/preise" class="nav-link w-nav-link">Preise</a>
+        <a href="/kontakt" class="nav-link w-nav-link">Kontakt</a>
         <a href="tel:+495722913800" class="button-cta w-button">+49 5722 913800</a>
       </nav>
       <div class="menu-button w-nav-button">
@@ -111,7 +111,7 @@
   </div>
   <div data-w-id="6bb3502d-11a7-7305-cc26-615d8fce3ac0" class="section services wf-section">
     <div class="container max-1080-px">
-      <div class="parallax-image-wrapper"><img src="../images/bewertungen.png" sizes="(max-width: 479px) 91vw, (max-width: 767px) 95vw, (max-width: 1439px) 96vw, 1080px" srcset="../images/bewertungen-p-500.png 500w, ../images/bewertungen-p-800.png 800w, ../images/bewertungen-p-1080.png 1080w, ../images/bewertungen.png 1456w" alt="" class="parallax-image second-img-for-service-page"></div>
+      <div class="parallax-image-wrapper"><img src="/images/bewertungen.png" sizes="(max-width: 479px) 91vw, (max-width: 767px) 95vw, (max-width: 1439px) 96vw, 1080px" srcset="/images/bewertungen-p-500.png 500w, /images/bewertungen-p-800.png 800w, /images/bewertungen-p-1080.png 1080w, /images/bewertungen.png 1456w" alt="" class="parallax-image second-img-for-service-page"></div>
       <div class="w-richtext">
         <blockquote><strong><em>Warum ist eine genaue Immobilienbewertung unerlässlich?</em></strong></blockquote>
         <p><em>Eine genaue Immobilienbewertung ist unerlässlich aus zahlreichen Gründen. Sie bildet eine grundlegende Entscheidungsgrundlage, wenn es um den Kauf oder Verkauf einer Immobilie geht. Es ist für potenzielle Käufer und Verkäufer gleichermaßen wichtig, den wahren Wert einer Immobilie zu kennen, um sicherzustellen, dass sie nicht zu viel zahlen oder ihr Eigentum unter Wert verkaufen. Eine genaue Bewertung gibt Ihnen die notwendige Sicherheit und Transparenz, um eine fundierte Entscheidung zu treffen. Aber die Bedeutung einer genauen Immobilienbewertung geht noch weit darüber hinaus. Sie spielt auch eine entscheidende Rolle bei der langfristigen Planung und strategischen Entscheidungsfindung. Immobilieninvestoren benötigen präzise Bewertungen, um den potenziellen Ertrag ihrer Investitionen zu verstehen, Risiken zu minimieren und ihre Rendite zu maximieren. Darüber hinaus kann eine genaue Immobilienbewertung bei der Steuerplanung, beim Nachlassmanagement, bei der Aufnahme eines Hypothekendarlehens oder bei der Durchführung von Unternehmensbewertungen und -transaktionen unerlässlich sein. Bei all diesen Szenarien hilft eine präzise Immobilienbewertung dabei, Klarheit zu schaffen, Risiken zu verwalten und fundierte Entscheidungen zu treffen. Es ist daher von entscheidender Bedeutung, auf die Expertise von erfahrenen Profis wie Baucampus zu setzen, um eine genaue und zuverlässige Immobilienbewertung zu erhalten.</em></p>
@@ -148,11 +148,11 @@
             </div>
           </div>
           <div class="button-row">
-            <a href="../kontakt" class="button-cta flat w-button">Kontaktieren</a>
+            <a href="/kontakt" class="button-cta flat w-button">Kontaktieren</a>
           </div>
         </div>
         <div class="layout-image-wrapper">
-          <a href="#" class="lightbox-second-page w-inline-block w-lightbox"><img src="../images/squarebewertung.png" width="1326" sizes="(max-width: 479px) 91vw, (max-width: 767px) 95vw, (max-width: 1439px) 96vw, 1080px" srcset="../images/squarebewertung-p-500.png 500w, ../images/squarebewertung-p-800.png 800w, ../images/squarebewertung.png 1024w" alt="">
+          <a href="#" class="lightbox-second-page w-inline-block w-lightbox"><img src="/images/squarebewertung.png" width="1326" sizes="(max-width: 479px) 91vw, (max-width: 767px) 95vw, (max-width: 1439px) 96vw, 1080px" srcset="/images/squarebewertung-p-500.png 500w, /images/squarebewertung-p-800.png 800w, /images/squarebewertung.png 1024w" alt="">
             <script type="application/json" class="w-json">{
   "items": [
     {
@@ -304,13 +304,13 @@
       <div class="container">
         <div class="grid-card-social">
           <h3 id="w-node-_4aa0d7f3-2a4a-6a6b-69be-35f8e84120e3-e84120df">Baucampus in den Medien</h3>
-          <a id="w-node-_4aa0d7f3-2a4a-6a6b-69be-35f8e84120e5-e84120df" href="#" class="social-card center w-inline-block"><img src="../images/F2x_1F2x.png" id="w-node-_4aa0d7f3-2a4a-6a6b-69be-35f8e84120e6-e84120df" width="23" alt="">
+          <a id="w-node-_4aa0d7f3-2a4a-6a6b-69be-35f8e84120e5-e84120df" href="#" class="social-card center w-inline-block"><img src="/images/F2x_1F2x.png" id="w-node-_4aa0d7f3-2a4a-6a6b-69be-35f8e84120e6-e84120df" width="23" alt="">
             <div id="w-node-_4aa0d7f3-2a4a-6a6b-69be-35f8e84120e7-e84120df">
               <div class="card-name-text social">Baucampus Deutschland</div>
               <p>Unserer Seite beitreten!</p>
             </div>
           </a>
-          <a id="w-node-_4aa0d7f3-2a4a-6a6b-69be-35f8e84120ec-e84120df" href="#" class="social-card w-inline-block"><img src="../images/T2x_1T2x.png" width="42" id="w-node-_4aa0d7f3-2a4a-6a6b-69be-35f8e84120ed-e84120df" alt="">
+          <a id="w-node-_4aa0d7f3-2a4a-6a6b-69be-35f8e84120ec-e84120df" href="#" class="social-card w-inline-block"><img src="/images/T2x_1T2x.png" width="42" id="w-node-_4aa0d7f3-2a4a-6a6b-69be-35f8e84120ed-e84120df" alt="">
             <div id="w-node-_4aa0d7f3-2a4a-6a6b-69be-35f8e84120ee-e84120df">
               <div class="card-name-text social">Baucampus Deutschland</div>
               <p>Folgen SIe uns!</p>
@@ -323,12 +323,12 @@
       <div class="container">
         <div class="footer-grid">
           <div id="w-node-_4aa0d7f3-2a4a-6a6b-69be-35f8e84120f6-e84120df" class="logo-wrapper fo-footer">
-            <div class="text-logo-wrapper for-footer"><img src="../images/logo.png" loading="lazy" width="50" height="50" srcset="../images/logo-p-500.png 500w, ../images/logo-p-800.png 800w, ../images/logo-p-1080.png 1080w, ../images/logo.png 1172w" sizes="50px" alt="">
+            <div class="text-logo-wrapper for-footer"><img src="/images/logo.png" loading="lazy" width="50" height="50" srcset="/images/logo-p-500.png 500w, /images/logo-p-800.png 800w, /images/logo-p-1080.png 1080w, /images/logo.png 1172w" sizes="50px" alt="">
               <div class="logo-text">Baucampus</div>
               <div class="logo-text subtext">München</div>
             </div>
           </div>
-          <p id="w-node-_4aa0d7f3-2a4a-6a6b-69be-35f8e84120fd-e84120df" class="footer-text copyright">© 2023 Baucampus by <a href="https://ariet.is" target="_blank">Arietis </a>- <a href="../legal/impressum">Impressum</a> - <a href="../legal/datenschutz">Datenschutz</a>
+          <p id="w-node-_4aa0d7f3-2a4a-6a6b-69be-35f8e84120fd-e84120df" class="footer-text copyright">© 2023 Baucampus by <a href="https://ariet.is" target="_blank">Arietis </a>- <a href="/legal/impressum">Impressum</a> - <a href="/legal/datenschutz">Datenschutz</a>
           </p>
           <div id="w-node-_4aa0d7f3-2a4a-6a6b-69be-35f8e8412105-e84120df">
             <a href="tel:+35391760711" class="list-item-footer w-inline-block">
@@ -345,6 +345,6 @@
     </section>
   </footer>
   <script src="https://d3e54v103j8qbb.cloudfront.net/js/jquery-3.5.1.min.dc5e7f18c8.js?site=648d0891096d52bfd9f271dc" type="text/javascript" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
-  <script src="../js/webflow.js" type="text/javascript"></script>
+  <script src="/js/webflow.js" type="text/javascript"></script>
 </body>
 </html>
