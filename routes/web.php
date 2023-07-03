2d521fa59e2ec463b2fae7b5c7009917a6caef2c
Oskar Lohse <oskar@baucampus.de>
2023-07-04 00:03:51 +0200

    test

--- routes/web.php
+++ routes/web.php
@@ -71,7 +71,7 @@ Route::domain($domain)->group(function () use ($routes, $domainData) {
         View::share('data', $data);
         return view('index', ['expert' => $expert,'domainort' => $domainData['domainort']]);
     });
-    Route::get('/{ort}/landingpage', [OrteatController::class, 'show'], function () use ($domainData) {})
+    Route::get('/{ort}', [OrteatController::class, 'show'], function () use ($domainData) {})
             ->middleware('cache.headers:private;max_age=3600');
 
 foreach ($routes as $route) {
