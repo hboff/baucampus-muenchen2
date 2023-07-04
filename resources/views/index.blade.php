a748592423d0ffa01c56d0984404e45ac03c0c28
Oskar Lohse <oskar@baucampus.de>
2023-07-04 21:45:18 +0200

    navigation

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -94,12 +94,11 @@ $p = 0;
 @if ($p++ == 1)
 <a href="tel:{{$row->Phone}}" class="button-cta w-button">{{$row->Phone}}</a> 
 @endif
-@else
+@endif
+@if ($p == 0)
 <a href="tel:+495722913800" class="button-cta w-button">+49 5722 913800</a>
-@break
 @endif
 @endforeach
-
       </nav>
       <div class="menu-button w-nav-button">
         <div class="w-icon-nav-menu"></div>
@@ -173,6 +172,9 @@ $p = 0;
 @if ($p++ == 1)
 <a href="tel:{{$row->Phone}}" class="button-cta w-button">{{$row->Phone}}</a> 
 @endif
+@endif
+@if ($p == 0)
+<a href="tel:+495722913800" class="button-cta w-button">+49 5722 913800</a>
 @endif
 @endforeach
 
--- resources/views/schimmelpilz.blade.php
+++ resources/views/schimmelpilz.blade.php
@@ -94,12 +94,11 @@ $p = 0;
 @if ($p++ == 1)
 <a href="tel:{{$row->Phone}}" class="button-cta w-button">{{$row->Phone}}</a> 
 @endif
-@else
+@endif
+@if ($p == 0)
 <a href="tel:+495722913800" class="button-cta w-button">+49 5722 913800</a>
-@break
 @endif
 @endforeach
-
       </nav>
       <div class="menu-button w-nav-button">
         <div class="w-icon-nav-menu"></div>
@@ -176,12 +175,11 @@ $p = 0;
 @if ($p++ == 1)
 <a href="tel:{{$row->Phone}}" class="button-cta w-button">{{$row->Phone}}</a> 
 @endif
-@else
+@endif
+@if ($p == 0)
 <a href="tel:+495722913800" class="button-cta w-button">+49 5722 913800</a>
-@break
 @endif
 @endforeach
-
           </nav>
           <div class="menu-button w-nav-button">
             <div class="w-icon-nav-menu"></div>
@@ -249,9 +247,8 @@ $p = 0;
             <a href="https://baucampus.com" target="_blank" class="nav-link w-nav-link">Baucampus</a>
             <a href="/preise" class="nav-link w-nav-link">Preise</a>
             <a href="/kontakt" class="nav-link w-nav-link">Kontakt</a>
-            @php
+@php
 $p = 0;
-
 @endphp
 @foreach ($expert as $row)
 @if($domainort == $row->stadt)
@@ -337,12 +334,11 @@ $p = 0;
 @if ($p++ == 1)
 <a href="tel:{{$row->Phone}}" class="button-cta w-button">{{$row->Phone}}</a> 
 @endif
-@else
+@endif
+@if ($p == 0)
 <a href="tel:+495722913800" class="button-cta w-button">+49 5722 913800</a>
-@break
 @endif
 @endforeach
-
           </nav>
           <div class="menu-button w-nav-button">
             <div class="w-icon-nav-menu"></div>
