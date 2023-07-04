dbf7afd2632e2d308c5c8c9e8b48a296edf9846d
Oskar Lohse <oskar@baucampus.de>
2023-07-04 10:49:45 +0200

    footer landingpage

--- resources/views/partials/footer-landingpage.blade.php
+++ resources/views/partials/footer-landingpage.blade.php
@@ -3,13 +3,13 @@
       <div class="container">
         <div class="grid-card-social">
           <h3 id="w-node-_4aa0d7f3-2a4a-6a6b-69be-35f8e84120e3-e84120df">Baucampus in den Medien</h3>
-          <a id="w-node-_4aa0d7f3-2a4a-6a6b-69be-35f8e84120e5-e84120df" href="#" class="social-card center w-inline-block"><img src="images/F2x_1F2x.png" id="w-node-_4aa0d7f3-2a4a-6a6b-69be-35f8e84120e6-e84120df" width="23" alt="">
+          <a id="w-node-_4aa0d7f3-2a4a-6a6b-69be-35f8e84120e5-e84120df" href="#" class="social-card center w-inline-block"><img src="/images/F2x_1F2x.png" id="w-node-_4aa0d7f3-2a4a-6a6b-69be-35f8e84120e6-e84120df" width="23" alt="">
             <div id="w-node-_4aa0d7f3-2a4a-6a6b-69be-35f8e84120e7-e84120df">
               <div class="card-name-text social">Baucampus Deutschland</div>
               <p>Unserer Seite beitreten!</p>
             </div>
           </a>
-          <a id="w-node-_4aa0d7f3-2a4a-6a6b-69be-35f8e84120ec-e84120df" href="#" class="social-card w-inline-block"><img src="images/T2x_1T2x.png" width="42" id="w-node-_4aa0d7f3-2a4a-6a6b-69be-35f8e84120ed-e84120df" alt="">
+          <a id="w-node-_4aa0d7f3-2a4a-6a6b-69be-35f8e84120ec-e84120df" href="#" class="social-card w-inline-block"><img src="/images/T2x_1T2x.png" width="42" id="w-node-_4aa0d7f3-2a4a-6a6b-69be-35f8e84120ed-e84120df" alt="">
             <div id="w-node-_4aa0d7f3-2a4a-6a6b-69be-35f8e84120ee-e84120df">
               <div class="card-name-text social">Baucampus Deutschland</div>
               <p>Folgen SIe uns!</p>
@@ -22,7 +22,7 @@
       <div class="container">
         <div class="footer-grid">
           <div id="w-node-_4aa0d7f3-2a4a-6a6b-69be-35f8e84120f6-e84120df" class="logo-wrapper fo-footer">
-            <div class="text-logo-wrapper for-footer"><img src="images/logo.png" loading="lazy" width="50" height="50" srcset="images/logo-p-500.png 500w, images/logo-p-800.png 800w, images/logo-p-1080.png 1080w, images/logo.png 1172w" sizes="50px" alt="">
+            <div class="text-logo-wrapper for-footer"><img src="/images/logo.png" loading="lazy" width="50" height="50" srcset="/images/logo-p-500.png 500w, /images/logo-p-800.png 800w, /images/logo-p-1080.png 1080w, /images/logo.png 1172w" sizes="50px" alt="">
               <div class="logo-text">Baucampus</div>
               <div class="logo-text subtext">München</div>
             </div>
