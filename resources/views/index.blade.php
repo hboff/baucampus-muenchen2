76bde9de76adb8e01d10c6ee74efc34fdd11b477
Oskar Lohse <oskar@baucampus.de>
2023-07-04 13:38:53 +0200

    phone

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -93,6 +93,8 @@ $p = 0;
 @if($domainort == $row->stadt)
 @if ($p++ == 1)
         <a href="tel:{{$row->Phone}}" class="button-cta w-button">{{$row->Phone}}</a>
+        @else
+        <a href="tel:+495722913800" class="button-cta w-button">+49 5722 913800</a>
         @endif
         @endif
         @endforeach
