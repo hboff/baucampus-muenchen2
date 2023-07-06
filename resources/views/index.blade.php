83e0b41960519f3a3f4fad08f4d928437484a949
Oskar Lohse <oskar@baucampus.de>
2023-07-06 11:25:14 +0200

    index 2

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -845,7 +845,7 @@ $p = 0;
       </div>
     </div>
   </section>
-  @include(partials.footer)
+  @include('partials.footer')
   <script src="https://d3e54v103j8qbb.cloudfront.net/js/jquery-3.5.1.min.dc5e7f18c8.js?site=648d0891096d52bfd9f271dc" type="text/javascript" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
   <script src="js/webflow.js" type="text/javascript"></script>
 </body>
